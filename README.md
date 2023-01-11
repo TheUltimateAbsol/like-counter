fa61e4becae3d7a02e3a824504cc764d2bfe825a
TheUltimateAbsol <thebellis618@gmail.com>
2023-01-11 17:24:33 -0500

    Fixed scripts, added views

--- README.md
+++ README.md
@@ -7,5 +7,7 @@ pip3 install snscrape
 
 Run as a cronjob:
 ```
-*/15 * * * * cd /data/data/com.termux/files/home/like-counter && git pull && /data/data/com.termux/files/usr/bin/bash update.sh && git add index.html likes.txt followers.txt && git commit -m "data update" && git push >/dev/null 2>&1
+*/15 * * * * cd /data/data/com.termux/files/home/like-counter && git pull && /data/data/com.termux/files/usr/bin/bash update.sh 20 && git add index.html likes.txt followers.txt && git commit -m "data update" && git push >/dev/null 2>&1
+0 1 * * * cd /data/data/com.termux/files/home/like-counter && git pull && /data/data/com.termux/files/usr/bin/bash update.sh 100 && git add index.html likes.txt followers.txt && git commit -m "data update" && git push >/dev/null 2>&1
+0 0 1 * * cd /data/data/com.termux/files/home/like-counter && git pull && /data/data/com.termux/files/usr/bin/bash update.sh 3200 && git add index.html likes.txt followers.txt && git commit -m "data update" && git push >/dev/null 2>&1
 ```
--- update.sh
+++ update.sh
@@ -1,15 +1,23 @@
-declare -i LIKES=$(snscrape --jsonl twitter-user alkalinedd | jq -n '[inputs.likeCount] | add' | xargs echo -n)
-declare -i FOLLOWERS=$(snscrape --max-results 0 --jsonl --with-entity twitter-user alkalinedd | jq -n 'inputs.followersCount' | xargs echo -n)
+declare -i LIKES
+declare -i VIEWS
+read -r LIKES VIEWS < <(python3 get-tweets.py $1)
+declare -i FOLLOWERS=$(python3 get-followers.py)
 declare -i OLD_LIKES=$(tail -n 1 likes.txt | xargs echo -n)
 declare -i OLD_FOLLOWERS=$(tail -n 1 followers.txt | xargs echo -n)
+declare -i OLD_VIEWS=$(tail -n 1 views.txt | xargs echo -n)
 declare -i LIKES_DIFFERENCE=$(expr $LIKES - $OLD_LIKES)
 declare -i FOLLOWERS_DIFFERENCE=$(expr $FOLLOWERS - $OLD_FOLLOWERS)
-echo $LIKES $FOLLOWERS $OLD_LIKES $OLD_FOLLOWERS $LIKES_DIFFERENCE $FOLLOWERS_DIFFERENCE
-sed -i "15s/.*/\t\t$LIKES/" index.html
-sed -i "22s/.*/\t\t$FOLLOWERS/" index.html
-sed -i "17s/.*/\t\t\t$LIKES_DIFFERENCE/" index.html
-sed -i "24s/.*/\t\t\t$FOLLOWERS_DIFFERENCE/" index.html
+declare -i VIEWS_DIFFERENCE=$(expr $VIEWS - $OLD_VIEWS)
+echo $LIKES $FOLLOWERS $VIEWS $OLD_LIKES $OLD_FOLLOWERS $OLD_VIEWS $LIKES_DIFFERENCE $FOLLOWERS_DIFFERENCE $VIEWS_DIFFERENCE
+sed -i "36s/.*/\t\t$LIKES/" index.html
+sed -i "45s/.*/\t\t$FOLLOWERS/" index.html
+sed -i "54s/.*/\t\t$VIEWS/" index.html
+sed -i "38s/.*/\t\t\t$LIKES_DIFFERENCE/" index.html
+sed -i "47s/.*/\t\t\t$FOLLOWERS_DIFFERENCE/" index.html
+sed -i "56s/.*/\t\t\t$VIEWS_DIFFERENCE/" index.html
 sed -i "1i $LIKES" likes.txt
 sed -i "1i $FOLLOWERS" followers.txt
+sed -i "1i $VIEWS" views.txt
 sed -i '$d' likes.txt
 sed -i '$d' followers.txt
+sed -i '$d' views.txt
